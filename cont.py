1f04cd0c3fae9902511d8eeb6a227a5bfc4ac4b9
Jon Mockbee <mockbee1000@gmail.com>
2021-08-01 17:27:31 -0400

    working through it

--- cont.py
+++ cont.py
@@ -1,5 +1,14 @@
 # Prompt the user to enter a word
 # and assign it to the user_word variable.
 
-for letter in user_word:
+#for letter in user_word:
     # Complete the body of the for loop.
+
+for letter in 'Python':     # First Example
+    if letter == 'y,h,o':
+        continue
+    
+print ('Current Letter :', letter)
+
+
+
